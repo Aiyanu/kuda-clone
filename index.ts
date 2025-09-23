d17298b4b6b8d0cfc944ff3a8d3cabaa447a53bd
Aiyanu <aiyanu1.00@gmail.com>
2025-09-23 02:49:19 +0100

    updating url in docs

--- index.ts
+++ index.ts
@@ -36,7 +36,9 @@ const swaggerOptions = {
     },
     servers: [
       {
-        url: "http://localhost:" + (process.env.PORT || 5000),
+        url:
+          process.env.APP_URL ||
+          "http://localhost:" + (process.env.PORT || 5000),
       },
     ],
   },
